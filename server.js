8217b193ec615fcac1cc66be4e43ad45ac63be09
Blessing <bigbadumhe@gmail.com>
2022-05-10 00:09:01 -0500

    route front end

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const PORT = 4000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`you are listening on ${PORT}`);
+});
